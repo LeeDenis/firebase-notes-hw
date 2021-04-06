fe142b60ac30768aaa5b93a4c8afff1739625952
LeeDenis <79125449+LeeDenis@users.noreply.github.com>
2021-04-06 16:55:24 -0400

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyAd4v2Y2o6r3piY-yWAZ1oJ1rao0oWcimM",
+    authDomain: "win-firebase-chat-hw.firebaseapp.com",
+    databaseURL: "https://win-firebase-chat-hw-default-rtdb.firebaseio.com/",
+    projectId: "win-firebase-chat-hw",
+    storageBucket: "win-firebase-chat-hw.appspot.com",
+    messagingSenderId: "4783848992",
+    appId: "1:4783848992:web:fcca4c4e8602f996ef4645",
+    measurementId: "G-32VYDK9RHL"
+  };
